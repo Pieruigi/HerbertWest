3c8e7bd4890162cdbfa8e2d81a63c3cb89d1f064
Pieruigi <pierluigi.zomparelli@gmail.com>
2021-01-18 23:27:33 +0100

    inventory UI wip

---
+++ Assets/Scripts/_Test/TestInventory.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zom.Pie.UI;
+using Zom.Pie;
+using Zom.Pie.Collections;
+
+public class TestInventory : MonoBehaviour
+{
+    [SerializeField]
+    List<Item> items;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Item item in items)
+        {
+            Inventory.Instance.Add(item);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if(InventoryUI.Instance.IsOpen())
+            {
+                InventoryUI.Instance.Close();
+            }
+            else
+            {
+                InventoryUI.Instance.Open();
+            }
+        }
+    }
+}
