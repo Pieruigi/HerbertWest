aa6230fe5b16abf562d53a0a107dd8f560d7a2d0
Pieruigi <pierluigi.zomparelli@gmail.com>
2021-02-09 18:50:18 +0100

    chess wip

--- Assets/Scripts/Interaction/Puzzles/ChessPuzzleController.cs
+++ Assets/Scripts/Interaction/Puzzles/ChessPuzzleController.cs
@@ -7,9 +7,57 @@ namespace Zom.Pie
     public class ChessPuzzleController : PuzzleController
     {
         
+        [SerializeField]
+        List<GameObject> pieces;
+
+        
+        protected override void Start()
+        {
+            base.Start();
+
+            if(finiteStateMachine.CurrentStateId == CompletedState)
+            {
+                // Set the chess open
+            }
+            else
+            {
+                // Set buttons 
+                
+                for(int i=0; i<pieces.Count; i++)
+                {
+                    Vector3 pos = pieces[i].transform.position;
+                    if (i == 0) // Is the horse
+                    {
+                        pos.y += 0.02f;
+                    }
+                    else
+                    {
+                        pos.y -= 0.018f;
+                    }
+
+                    pieces[i].transform.position = pos;
+                }
+            }
+        }
+
+        bool busy = false;
         public override void Interact(Interactor interactor)
         {
+            if (busy)
+                return;
+
+            busy = true;
+
             Debug.Log("Interacting with " + interactor.gameObject);
+        }
+
+        IEnumerator DoInteraction()
+        {
+            OnPuzzleInteractionStart?.Invoke(this);
+
+            yield return new WaitForSeconds(1f);
+
+            OnPuzzleInteractionStop?.Invoke(this);
         }
     }
 
