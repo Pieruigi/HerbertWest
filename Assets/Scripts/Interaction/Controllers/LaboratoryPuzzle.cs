dbb150941b8eedd6f62ed2eeb4f221572ae42d83
Pieruigi <pierluigi.zomparelli@gmail.com>
2021-05-28 11:38:14 +0200

    puzzle lab

---
+++ Assets/Scripts/Interaction/Controllers/LaboratoryPuzzle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zom.Pie
+{
+    public class LaboratoryPuzzle : PuzzleController
+    {
+        public override void Interact(Interactor interactor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+
+}
